325abdad3357bcebe45fd0a2894297a399ee0cd5
smpol <5789000+smpol@users.noreply.github.com>
2024-04-04 10:15:35 +0200

    test nieudany

--- src/main/java/com/example/workshops/ExampleController.java
+++ src/main/java/com/example/workshops/ExampleController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ExampleController {
+public clas ExampleController {
 
     @GetMapping("/hello")
     public String hello() {
